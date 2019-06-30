90357ac4712e80cc16ac8affbe3f0e448ba17065
Oleg Bruev <job4bruev@gmail.com>
2019-06-30 20:43:28 +0300

    LICENSE

--- Components/Store/Part/Viewer.cs
+++ Components/Store/Part/Viewer.cs
@@ -1,19 +1,20 @@
 ﻿/*
-    OrderMaker - http://ordermaker.org
-    Copyright(c) 2019 Oleg Bruev. All rights reserved.
+    MTD OrderMaker - http://ordermaker.org
+    Copyright (c) 2019 Oleg Bruev <job4bruev@gmail.com>. All rights reserved.
 
-    This program is free software: you can redistribute it and/or modify
+    This file is part of MTD OrderMaker.
+    MTD OrderMaker is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.If not, see https://www.gnu.org/licenses/.
+    along with this program.  If not, see  https://www.gnu.org/licenses/.
 */
 
 using Microsoft.AspNetCore.Mvc;
